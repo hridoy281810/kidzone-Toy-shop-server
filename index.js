b14a47afaad5c6dfdf6b5cf8a07d84ffd182042a
hridoy281810 <hridoy01724281810@gmail.com>
2023-05-19 12:42:59 +0600

    create a get api data show category wise

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.POST || 5000;
 
+ console.log(process.env.DB_USER)
+ console.log(process.env.DB_PASS)
 app.use(cors())
 app.use(express.json())
 
@@ -27,6 +29,113 @@ async function run() {
     await client.connect();
     
      const toyCollection = client.db('toyShop').collection('toyDb')
+     console.log(toyCollection)
+    
+    const indexKeys = {toyName: 1};
+    const indexOption = {name:'toyName'} ;
+    const result = await toyCollection.createIndex(indexKeys,indexOption)
+
+//  search with toy name 
+    app.get('/toyNameSearch/:text', async(req,res)=>{
+      const searchText = req.params.text;
+      const result = await toyCollection.find({
+        $or:[
+          {toyName: {$regex: searchText, $options: 'i'}}
+        ]}).toArray()
+        res.send(result)
+    })
+
+
+    app.get('/allToys', async(req,res)=>{
+      const result= await toyCollection.find().limit(20).toArray()
+      res.send(result)
+    })
+
+
+    //  tab with toy category name 
+     app.get('/allToys/:category', async(req,res)=>{
+      console.log(req.params.category )
+      if(req.params.category === 'Teddy Bear' || req.params.category === 'Horse' || req.params.category === 'Cat'){
+        const result = await toyCollection.find({category: req.params.category}).toArray()
+        return res.send(result)
+      }
+        const result = await toyCollection.find().toArray()
+        return res.send(result)
+     })
+
+
+    //  post toy api 
+     app.post('/postToy', async(req,res)=>{
+        const body = req.body;
+        if(!body){
+            return res.status(404).send({message: 'body not found'})
+        }
+        const result = await toyCollection.insertOne(body)
+        res.send(result)
+     })
+
+    //  privet route with email 
+      app.get('/myToys/:email' , async(req,res)=>{
+      //  console.log(req.params.email)
+      //  const type = req.query.type === 'ascending';
+      //  const value = req.query.value;
+      //  const sortData = {}
+      //  sortData[value]= type ? 1: -1;
+       const result = await  toyCollection.find( {email : req.params.email} ).sort({price: -1}).toArray();
+       res.send(result)
+    })
+     
+ 
+     
+    // pictureUrl,
+    // toyName,
+    // price,
+    // rating,
+    // availableQuantity,
+    // detailDescription,
+    // update api
+    app.put('/update/:id', async(req,res)=>{
+      const id = req.params.id;
+      const user = req.body;
+      console.log(id,user)
+      const filter = {_id: new ObjectId(id)}
+      const option = {upsert: true}
+      const updateDoc = {
+     $set: {
+          pictureUrl: user.pictureUrl,
+          toyName: user.toyName,
+          price: user.price,
+          rating: user.rating,
+          availableQuantity: user.availableQuantity,
+          detailDescription: user.detailDescription,
+        },
+      }
+      const result = await toyCollection.updateOne(filter,updateDoc,option)
+      res.send(result)
+    })
+
+
+    // all toy by  id
+    app.get('/allToy/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      // const options = {
+      //   projection: { category: 1, price: 1,_id:1, pictureUrl: 1, toyName: 1, rating: 1,seller:1, availableQuantity: 1, sellerEmail: 1,detailDescription:1},
+      // };
+    
+      const result = await toyCollection.findOne(query)
+      res.send(result)
+    })
+
+    // delete api 
+    app.delete('/myToys/:id', async(req,res)=>{
+      const id = req.params.id;
+      console.log('please delete',id)
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.deleteOne(query)
+      res.send(result)
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
