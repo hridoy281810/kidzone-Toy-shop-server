5c37639fb162998cef9c6bd0848ba3a74ad11489
hridoy281810 <hridoy01724281810@gmail.com>
2023-05-19 19:24:09 +0600

    creat a dynamic api with email

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.POST || 5000;
 
- console.log(process.env.DB_USER)
- console.log(process.env.DB_PASS)
 app.use(cors())
 app.use(express.json())
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6iur0l.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,76 +23,67 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-     const toyCollection = client.db('toyShop').collection('toyDb')
-     console.log(toyCollection)
-    
-    const indexKeys = {toyName: 1};
-    const indexOption = {name:'toyName'} ;
-    const result = await toyCollection.createIndex(indexKeys,indexOption)
 
-//  search with toy name 
-    app.get('/toyNameSearch/:text', async(req,res)=>{
+    const toyCollection = client.db('toyShop').collection('toyDb')
+    console.log(toyCollection)
+
+    const indexKeys = { toyName: 1 };
+    const indexOption = { name: 'toyName' };
+    const result = await toyCollection.createIndex(indexKeys, indexOption)
+
+    //  search with toy name 
+    app.get('/toyNameSearch/:text', async (req, res) => {
       const searchText = req.params.text;
       const result = await toyCollection.find({
-        $or:[
-          {toyName: {$regex: searchText, $options: 'i'}}
-        ]}).toArray()
-        res.send(result)
-    })
-
-
-    app.get('/allToys', async(req,res)=>{
-      const result= await toyCollection.find().limit(20).toArray()
+        $or: [
+          { toyName: { $regex: searchText, $options: 'i' } }
+        ]
+      }).toArray()
       res.send(result)
     })
 
+    // all toys api 
+    app.get('/allToys', async (req, res) => {
+      const result = await toyCollection.find().limit(20).toArray()
+      res.send(result)
+    })
 
     //  tab with toy category name 
-     app.get('/allToys/:category', async(req,res)=>{
-      console.log(req.params.category )
-      if(req.params.category === 'Teddy Bear' || req.params.category === 'Horse' || req.params.category === 'Cat'){
-        const result = await toyCollection.find({category: req.params.category}).toArray()
+    app.get('/allToys/:category', async (req, res) => {
+      console.log(req.params.category)
+      if (req.params.category === 'Teddy Bear' || req.params.category === 'Horse' || req.params.category === 'Cat') {
+        const result = await toyCollection.find({ category: req.params.category }).toArray()
         return res.send(result)
       }
-        const result = await toyCollection.find().toArray()
-        return res.send(result)
-     })
-
+      const result = await toyCollection.find().toArray()
+      return res.send(result)
+    })
 
     //  post toy api 
-     app.post('/postToy', async(req,res)=>{
-        const body = req.body;
-        if(!body){
-            return res.status(404).send({message: 'body not found'})
-        }
-        const result = await toyCollection.insertOne(body)
-        res.send(result)
-     })
+    app.post('/postToy', async (req, res) => {
+      const body = req.body;
+      if (!body) {
+        return res.status(404).send({ message: 'body not found' })
+      }
+      const result = await toyCollection.insertOne(body)
+      res.send(result)
+    })
 
     //  privet route with email 
-      app.get('/myToys/:email' , async(req,res)=>{
-       const result = await  toyCollection.find( {email : req.params.email} ).sort({price: -1}).toArray();
-       res.send(result)
+    app.get('/myToys/:email', async (req, res) => {
+      const result = await toyCollection.find({ email: req.params.email }).sort({ price: -1 }).toArray();
+      res.send(result)
     })
-     
- 
-     
-    // pictureUrl,
-    // toyName,
-    // price,
-    // rating,
-    // availableQuantity,
-    // detailDescription,
-    // update api
-    app.put('/update/:id', async(req,res)=>{
+
+    //  update api 
+    app.put('/update/:id', async (req, res) => {
       const id = req.params.id;
       const user = req.body;
-      console.log(id,user)
-      const filter = {_id: new ObjectId(id)}
-      const option = {upsert: true}
+      console.log(id, user)
+      const filter = { _id: new ObjectId(id) }
+      const option = { upsert: true }
       const updateDoc = {
-     $set: {
+        $set: {
           pictureUrl: user.pictureUrl,
           toyName: user.toyName,
           price: user.price,
@@ -105,28 +92,23 @@ async function run() {
           detailDescription: user.detailDescription,
         },
       }
-      const result = await toyCollection.updateOne(filter,updateDoc,option)
+      const result = await toyCollection.updateOne(filter, updateDoc, option)
       res.send(result)
     })
 
-
     // all toy by  id
-    app.get('/allToy/:id', async(req,res)=>{
+    app.get('/allToy/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      // const options = {
-      //   projection: { category: 1, price: 1,_id:1, pictureUrl: 1, toyName: 1, rating: 1,seller:1, availableQuantity: 1, sellerEmail: 1,detailDescription:1},
-      // };
-    
+      const query = { _id: new ObjectId(id) }
       const result = await toyCollection.findOne(query)
       res.send(result)
     })
 
     // delete api 
-    app.delete('/myToys/:id', async(req,res)=>{
+    app.delete('/myToys/:id', async (req, res) => {
       const id = req.params.id;
-      console.log('please delete',id)
-      const query = {_id: new ObjectId(id)}
+      console.log('please delete', id)
+      const query = { _id: new ObjectId(id) }
       const result = await toyCollection.deleteOne(query)
       res.send(result)
 
@@ -141,12 +123,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-app.get('/', (req,res)=>{
-    res.send('toy shop server is running')
+//  surver running test
+app.get('/', (req, res) => {
+  res.send('toy shop server is running')
 })
-app.listen(port,()=>{
-    console.log(`Toy shop server is running on PORT: ${port}`)
+app.listen(port, () => {
+  console.log(`Toy shop server is running on PORT: ${port}`)
 })
